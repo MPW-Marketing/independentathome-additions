dd6af71d0222c0cc73d12766cc7932a94bb9aa2e
damonmaldonado <damon@mpwmarketing.com>
2016-04-08 15:24:26 -0400

    Create independent-additions.php

---
+++ independent-additions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Independent At Home Additions
+Description: Additional Functionality
+Version: 0.1.0
+Author: mpw
+Text Domain: independent-at-home
+*/
+
+add_filter('widget_text', 'do_shortcode');
